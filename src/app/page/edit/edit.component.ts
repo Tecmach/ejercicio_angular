fd59b9bd574b14e593fcd2d517311fd11dd7f085
Tecmach <josecamlopez@hotmail.com>
2022-11-05 13:22:56 -0700

    .prueba1_no muestra datos en form

--- src/app/page/edit/edit.component.ts
+++ src/app/page/edit/edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RegistrosService, Registro } from '../../services/registros.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
   id:string="";
-  constructor() { }
+  registroActual: Registro={Id_registro:'',CURP:'',Nombre:'', primer_apellido:'', segundo_apellido:'', instituto:''}
+  constructor(private RegistrosService: RegistrosService, private activateRouter: ActivatedRoute, private route:Router) { }
 
   ngOnInit(): void {
+    this.id = this.activateRouter.snapshot.params['id'];
+    this.RegistrosService.getunRegistro(this.id).subscribe(
+      res=>{this.registroActual=res, console.log(this.registroActual)},
+      err=> console.log(err)
+    )
+  }
 
+  guardar(){
+    this.RegistrosService.editunRegistro(this.id, this.registroActual).subscribe(
+      res=>this.route.navigate(['/home']),
+      err=> console.log(err)
+    )
   }
 }
