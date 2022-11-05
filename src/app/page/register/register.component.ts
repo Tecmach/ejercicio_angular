9bb87bcf837e76171a3a2ec2fce2d3adea623edc
Tecmach <josecamlopez@hotmail.com>
2022-11-05 11:47:40 -0700

    base inicial

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -1,21 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {Registro,RegistrosService} from "../../services/registros.service";
-import {Router} from "@angular/router";
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registroNuevo: Registro={id_registro:'',curp:'', nombre:'', apellido:'', apellido1:'', instituto:''};
-  constructor(private RegistrosService: RegistrosService, private router: Router) { }
+
+  constructor() { }
 
   ngOnInit(): void {
   }
-  agregarRegistro(){
-    this.RegistrosService.addRegistro(this.registroNuevo).subscribe(
-      res=> {console.log(res), this.router.navigate(['/inicio'])},
-      err=> console.log(err)
-    );
-  }
+
 }
